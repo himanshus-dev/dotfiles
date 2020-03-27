395b19c4c32a954db025ce2115c322ecbf7146d2
Himanshu Sharma <path2himanshu@gmail.com>
2020-03-27 06:16:29 +0000

    #TASK: tmux config  #comment Added tmux config to git #timestamp Mon Mar 23 03:42:36 UTC 2020

---
+++ tmux/README.md
@@ -0,0 +1,23 @@
+### Key-bindings for :  <span style="color:green;">tmux</span>
+
+| Key binding         |         Command         |
+| ------------------- | :---------------------: |
+| Ctrl + a            |         Command         |
+| Command + ?         |    Show key bindings    |
+| Command + n         |       New window        |
+| Command + f         |       Find window       |
+| Command +           |                         |
+| Command + w         |        Kill pane        |
+| Command + q         |      Kill session       |
+| Command + r         |   Reload tmux config    |
+| Command + z         |      Zoom to pane       |
+| Command + $         |     Rename session      |
+| Command + ,         |      Rename window      |
+| Command + g         |    Split vertically     |
+| Command + h         |   Split horizontally    |
+| Command + ?         | List keyboard shortcuts |
+| Command + :         |     Command prompt      |
+| Command + s         |      List sessions      |
+| Command + Ctrl + r  |     Reload session      |
+| Command + Ctrl + s  |      Save session       |
+| Command + Shift + i |     Install plugins     |
---
+++ tmux/setup.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+#######################################
+#          tmux setup script          #
+# ----------------------------------- #
+# This installs required plugings and #
+# sets the conf file to your home dir #
+# ----------------------------------- #
+#       Author : Himanshu Sharma      #
+#   Email: path2himanshu@gmail.com    #
+#######################################
+
+SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd) && cd "$SCRIPT_DIR" || return 1
+
+echo "SetupBot=> Setting tmux config, Master"
+
+# backup existing conf -- if exists
+[ -f ~/.tmux.conf ] && mv ~/.tmux.conf ~/.tmux.conf.bak
+
+# create a symlink to conf
+ln -sf "$SCRIPT_DIR/tmux.conf" ~/.tmux.conf
+
+
+echo "SetupBot=> Getting your plugings, Master"
+
+[[ ! -a ~/.tmux/plugins/tpm ]] && git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm # tpm - tmux plugin manager is
+[[ ! -a ~/.tmux/plugins/tmux-yank ]] && git clone https://github.com/tmux-plugins/tmux-yank.git ~/.tmux/plugins/tmux-yank
+[[ ! -a ~/.tmux/plugins/tmux-sensible ]] && git clone https://github.com/tmux-plugins/tmux-sensible.git ~/.tmux/plugins/tmux-sensible
+[[ ! -a ~/.tmux/plugins/tmux-resurrect ]] && git clone https://github.com/tmux-plugins/tmux-resurrect.git ~/.tmux/plugins/tmux-resurrect
+[[ ! -a ~/.tmux/plugins/tmux-continuum ]] && git clone https://github.com/tmux-plugins/tmux-continuum.git ~/.tmux/plugins/tmux-continuum
+[[ ! -a ~/.tmux/plugins/tmux-power ]] && git clone https://github.com/wfxr/tmux-power.git ~/.tmux/plugins/tmux-power
+[[ ! -a ~/.tmux/plugins/tmux-net-speed ]] && git clone https://github.com/wfxr/tmux-net-speed.git ~/.tmux/plugins/tmux-net-speed
+[[ ! -a ~/.tmux/plugins/tmux-prefix-highlight ]] && git clone https://github.com/wfxr/tmux-prefix-highlight.git ~/.tmux/plugins/tmux-prefix-highlight
+
+echo "SetupBot=> I'm done, Master"
